a6ff788aadd13e3e533e4ed00085400f145c31ce
np03cs4a240386-prog <np03cs4a240386@heraldcollege.edu.np>
2026-01-02 23:56:53 +0545

    Added GradeCalculator feature

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,4 +2,12 @@ package Workshop7;
 
 public class GradeCalculator {
 
+    public static double calculateAverage(int[] grades) {
+        int sum = 0;
+        for (int g : grades) {
+            sum += g;
+        }
+        return (double) sum / grades.length;
+    }
 }
+
